2118cb20813bdd052f81d549cdb57571b4f89722
jomzph <75261975+jomzph@users.noreply.github.com>
2022-07-10 22:10:01 +0800

    Flex and JS
    kulay

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function clickMe(){
+/* function clickMe(){
 
 document.querySelector(".name").style.backgroundColor = "red";
 
+}*/
 
+let x = 0
+
+function clickMe() {
+    if (x == 0) {
+        document.querySelector(".name").style.backgroundColor = "red"; 
+        document.querySelector(".name").innerHTML = "Joemar Menes POGI"
+        x = 1
+    }
+    else {
+        document.querySelector(".name").style.backgroundColor = "blue";
+        document.querySelector(".name").innerHTML = "Joemar Menes"
+        x = 0
+    }
 }
